817e33c9b1e929d5307603b9f99768a351ff2727
Breno Paiva <m.breno.op@gmail.com>
2026-07-05 14:00:38 -0300

    converter identacao para espacos

--- characters/base_character.gd
+++ characters/base_character.gd
@@ -9,27 +9,27 @@ class_name BaseCharacter
 @export var _sprite2D: Sprite2D
 
 func _process(_delta: float) -> void:
-	pass
+    pass
 
 func _physics_process(_delta):
-	_move()
-	_animate()
-	
+    _move()
+    _animate()
+    
 func _move() -> void:
-	var direction = Input.get_vector(
-		"move_left", "move_right", "move_up", "move_down"
-	)
+    var direction = Input.get_vector(
+        "move_left", "move_right", "move_up", "move_down"
+    )
 
-	velocity = direction * _move_speed
-	move_and_slide()
+    velocity = direction * _move_speed
+    move_and_slide()
 
 func _animate() -> void:
-	if velocity.x > 0:
-		_sprite2D.flip_h = false
-	if velocity.x < 0:
-		_sprite2D.flip_h = true
-	
-	if velocity:
-		_animation.play("walk")
-		return
-	_animation.play("idle")
+    if velocity.x > 0:
+        _sprite2D.flip_h = false
+    if velocity.x < 0:
+        _sprite2D.flip_h = true
+
+    if velocity:
+        _animation.play("walk")
+        return
+    _animation.play("idle")
--- ui/main_menu/menu.gd
+++ ui/main_menu/menu.gd
@@ -2,37 +2,37 @@ extends Control
 class_name MainMenu
 
 func _ready() -> void:
-	for _button in get_tree().get_nodes_in_group("buttons"):
-		_button.mouse_entered.connect(_on_mouse_entered.bind(_button))
-		_button.mouse_exited.connect(_on_mouse_exited.bind(_button))
-		_button.pressed.connect(_on_real_button_pressed.bind(_button))
+    for _button in get_tree().get_nodes_in_group("buttons"):
+        _button.mouse_entered.connect(_on_mouse_entered.bind(_button))
+        _button.mouse_exited.connect(_on_mouse_exited.bind(_button))
+        _button.pressed.connect(_on_real_button_pressed.bind(_button))
 
-	for _button in $MenuContainer/VBoxContainer.get_children():
-		if _button is NinePatchRect:
-			_button.mouse_entered.connect(_on_mouse_entered.bind(_button))
-			_button.mouse_exited.connect(_on_mouse_exited.bind(_button))
-			_button.gui_input.connect(_on_button_pressed.bind(_button))
-			
+    for _button in $MenuContainer/VBoxContainer.get_children():
+        if _button is NinePatchRect:
+            _button.mouse_entered.connect(_on_mouse_entered.bind(_button))
+            _button.mouse_exited.connect(_on_mouse_exited.bind(_button))
+            _button.gui_input.connect(_on_button_pressed.bind(_button))
+
 func _on_mouse_entered(_button) -> void:
-	_button.modulate.a = 0.5
-	
+    _button.modulate.a = 0.5
+
 func _on_mouse_exited(_button) -> void:
-	_button.modulate.a = 1.0
+    _button.modulate.a = 1.0
 
 func _on_button_pressed(_event, _button) -> void:
-	if _event is InputEventMouseButton:
-		if _event.button_index == 1 and _event.pressed:
-			match _button.name:
-				"PlayButton": 
-					get_tree().change_scene_to_file("res://management/game_level.tscn")
-					
-				"ExitButton":
-					get_tree().quit()
+    if _event is InputEventMouseButton:
+        if _event.button_index == 1 and _event.pressed:
+            match _button.name:
+                "PlayButton": 
+                    get_tree().change_scene_to_file("res://levels/game_level.tscn")
+
+                "ExitButton":
+                    get_tree().quit()
 
 func _on_real_button_pressed(_button) -> void:
-	match _button.name:
-		"CreditsButton":
-			$CreditsContainer.show()
+    match _button.name:
+        "CreditsButton":
+            $CreditsContainer.show()
 
-		"QuitCreditsButton":
-			$CreditsContainer.hide()
+        "QuitCreditsButton":
+            $CreditsContainer.hide()
