9c345999d2c7ee5b7d14236b9727ac124dcf62a4
Breno Paiva <m.breno.op@gmail.com>
2026-08-17 23:05:18 -0300

    finish theme configuration

--- src/ui/main_menu/menu.gd
+++ src/ui/main_menu/menu.gd
@@ -9,3 +9,6 @@ func _on_exit_pressed() -> void:
 
 func _on_credits_button_pressed() -> void:
     get_tree().change_scene_to_file("res://ui/credits.tscn")
+
+func _on_settings_button_pressed() -> void:
+    pass # TODO: Settings UI
