b8e522bcb00e75a5264a0719fee1fe6681792b43
Breno Paiva <m.breno.op@gmail.com>
2026-08-19 12:09:25 -0300

    update base_character

--- src/characters/base_character.gd
+++ src/characters/base_character.gd
@@ -8,28 +8,25 @@ class_name BaseCharacter
 @export var _animation: AnimationPlayer
 @export var _sprite2D: Sprite2D
 
-func _process(_delta: float) -> void:
-    pass
-
-func _physics_process(_delta):
-    _move()
-    _animate()
-    
+func _physics_process(_delta) -> void:
+	_move()
+	_animate()
+	
 func _move() -> void:
-    var direction = Input.get_vector(
-        "move_left", "move_right", "move_up", "move_down"
-    )
+	var direction = Input.get_vector(
+		"move_left", "move_right", "move_up", "move_down"
+	)
 
-    velocity = direction * _move_speed
-    move_and_slide()
+	velocity = direction * _move_speed
+	move_and_slide()
 
 func _animate() -> void:
-    if velocity.x > 0:
-        _sprite2D.flip_h = false
-    if velocity.x < 0:
-        _sprite2D.flip_h = true
+	if velocity.x > 0:
+		_sprite2D.flip_h = false
+	if velocity.x < 0:
+		_sprite2D.flip_h = true
 
-    if velocity:
-        _animation.play("walk")
-        return
-    _animation.play("idle")
+	if velocity:
+		_animation.play("walk")
+		return
+	_animation.play("idle")
