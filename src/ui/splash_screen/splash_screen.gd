0edefd4a5645e14efa8ed44af0df764e00a2b46f
Breno Paiva <m.breno.op@gmail.com>
2026-08-19 00:27:43 -0300

    update splash screen

--- src/ui/splash_screen/splash_screen.gd
+++ src/ui/splash_screen/splash_screen.gd
@@ -2,6 +2,6 @@ extends Control
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
     if anim_name == "fade_in":
-        $AnimationPlayer.play("fade_out")
+        $Transition.play("fade_out")
     if anim_name == "fade_out":
         get_tree().change_scene_to_file("res://ui/main_menu/main_menu.tscn")
